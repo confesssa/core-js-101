ff15e321ab7cebce75261e6a29dfecee7280293a
volha <v.khviadchenia@gmail.com>
2020-12-14 22:25:40 +0100

    feat: add solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -459,8 +459,16 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country === b.country) {
+      return a.city > b.city ? 1 : -1;
+    }
+    if (a.country > b.country) {
+      return 1;
+    }
+    return -1;
+  });
 }
 
 /**
